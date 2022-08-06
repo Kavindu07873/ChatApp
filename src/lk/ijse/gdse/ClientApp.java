b64a3352e9da4956ebcd7916d6ccd6073b839d5d
Kavindu07873 <Dilshankavindu07715@gmail.com>
2022-08-06 22:23:58 +0530

    clt msg ok

--- src/lk/ijse/gdse/ClientApp.java
+++ src/lk/ijse/gdse/ClientApp.java
@@ -1,11 +1,16 @@
 package lk.ijse.gdse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientApp {
     public static void main(String[] args) throws IOException {
         int PORT = 8000;
         Socket socket = new Socket("localhost",PORT);
+
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("Hello Socket, How are you ");
+        printWriter.flush();
     }
 }
--- src/lk/ijse/gdse/ServerApp.java
+++ src/lk/ijse/gdse/ServerApp.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,11 +14,15 @@ import java.net.Socket;
         System.out.println(PORT);
 
         Socket LocalSocket = serverSocket.accept();
+
         System.out.println(LocalSocket.getPort());
         System.out.println(LocalSocket.getInetAddress());
 
+        InputStreamReader inputStreamReader = new InputStreamReader(LocalSocket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-
+        String ClientMsg = bufferedReader.readLine();
+        System.out.println(ClientMsg);
     }
 }
 
